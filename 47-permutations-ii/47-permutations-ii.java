917486a0974360c369cdd5d11f4a3490103eee2f
Rohit <rv432233@gmail.com>
2022-05-10 01:32:37 +0530

    Time: 6 ms (45.84%), Space: 48.6 MB (33.40%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    List<List<Integer>> ans=new ArrayList<>();
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        HashMap<Integer,Integer> hs=new HashMap<>();
+        for(int i:nums)
+        {
+            hs.put(i,hs.getOrDefault(i,0)+1);
+        }
+        
+        perm(0,nums.length,new ArrayList<>(),hs);
+        return ans;
+    }
+    public void perm(int cs,int ts,List<Integer> li,HashMap<Integer,Integer> hs)
+    {
+        if(cs>=ts)
+        {
+            // System.out.println(li);
+            ans.add(new ArrayList<>(li));
+            return;
+        }
+            // System.out.println("sdnsak");
+        
+        for(int i:hs.keySet())
+        {
+            if(hs.get(i)>0)
+            {
+                hs.put(i,hs.get(i)-1);
+                li.add(i);
+                perm(cs+1,ts,li,hs);
+                li.remove(li.size()-1);
+                hs.put(i,hs.get(i)+1);
+            }
+            
+        }
+    }
+}
