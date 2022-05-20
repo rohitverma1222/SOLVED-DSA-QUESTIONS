492eb3fa1addd7e8d649a56f174bfc01411dad1f
Rohit <rv432233@gmail.com>
2022-05-20 18:26:05 +0530

    Time: 2 ms (99.30%), Space: 42.4 MB (83.05%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
@@ -14,96 +14,72 @@
  * }
  */
 class Solution {
-   // class pair implements Comparable<pair>{
-   //      TreeNode node;
-   //      int w;
-   //      int d;
-   //      pair(TreeNode node,int w,int d)
-   //      {
-   //          this.node=node;
-   //          this.w=w;
-   //          this.d=d;
-   //      }
-   //      public int CompareTo(pair other)
-   //      {
-   //          if(this.d==other.d)
-   //              return this.node.val-other.node.val;
-   //          else
-   //              return this.d-other.d;
-   //      }
-   //  }
-    class pair implements Comparable<pair> {
+    class pair implements Comparable<pair>{
         TreeNode node;
         int w;
         int d;
-
-        pair(TreeNode node, int w, int d){
-            this.node = node;
-            this.w = w;
-            this.d = d;
+        pair(TreeNode node,int w,int d)
+        {
+            this.node=node;
+            this.w=w;
+            this.d=d;
         }
-
-        // this - other means increasing
-        // other - this means decreasing
-        public int compareTo(pair other){
-            if(this.d == other.d){
-                return this.node.val - other.node.val;
-            } else {
-                return this.d - other.d;
-            }
+        public int compareTo(pair other)
+        {
+            if(this.d==other.d)
+                return this.node.val-other.node.val;
+            else
+                return this.d-other.d;
         }
     }
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-       
-        Deque<pair> ar=new LinkedList<>();
-        
-        ar.add(new pair(root,0,1));
-        
+        Queue<pair> mq=new LinkedList<>();
+        HashMap<Integer,PriorityQueue<pair>> hs=new HashMap<>();
+        mq.add(new pair(root,0,1));
         int l=Integer.MAX_VALUE;
         int r=Integer.MIN_VALUE;
-            HashMap<Integer,PriorityQueue<pair>> hs=new HashMap<>();
-        while(ar.size()>0)
+        while(mq.size()>0)
         {
-            pair rem=ar.remove();
+               pair rem=mq.remove();
             
             l=Math.min(l,rem.w);
             r=Math.max(r,rem.w);
             
-            
-            if(!hs.containsKey(rem.w))
+            if(hs.containsKey(rem.w))
+            {
+                hs.get(rem.w).add(rem);
+            }
+            else
             {
                 PriorityQueue<pair> pq=new PriorityQueue<>();
                 pq.add(rem);
                 hs.put(rem.w,pq);
             }
-            else
-            {
-                hs.get(rem.w).add(rem);
-            }
             
             if(rem.node.left!=null)
             {
-                ar.add(new pair(rem.node.left,rem.w-1,rem.d+1));
+                mq.add(new pair(rem.node.left,rem.w-1,rem.d+1));
             }
             if(rem.node.right!=null)
             {
-                ar.add(new pair(rem.node.right,rem.w+1,rem.d+1));
+                mq.add(new pair(rem.node.right,rem.w+1,rem.d+1));
             }
         }
-        
         List<List<Integer>> li=new ArrayList<>();
-        
-        for(int w=l;w<=r;w++)
+        for(int i=l;i<=r;i++)
         {
             List<Integer> ans=new ArrayList<>();
-            PriorityQueue<pair> pp=hs.get(w);
-            while(pp.size()!=0)
+            PriorityQueue<pair> pq=hs.get(i);
+            while(pq.size()!=0)
             {
-                pair some=pp.remove();
-                ans.add(some.node.val);
+                pair rem=pq.remove();
+                ans.add(rem.node.val);
             }
-            li.add(ans);
+            li.add(new ArrayList<>(ans));
         }
         return li;
     }
 }
+
+
+
