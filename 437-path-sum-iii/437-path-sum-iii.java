6179d7d15ac30a64f682fe768417c04e50490d54
Rohit <rv432233@gmail.com>
2022-05-29 13:00:51 +0530

    Time: 3 ms (95.98%), Space: 41.9 MB (86.84%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.java
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -1,18 +1,4 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
     int c=0;
     public int pathSum(TreeNode root, int tar) {
@@ -25,18 +11,19 @@ class Solution {
     }
     public void helper(TreeNode root,int sum,int tar,HashMap<Integer,Integer> hs)
     {
-        if(root==null)
-            return;
-        System.out.println(sum-tar);
+        
         if(hs.containsKey(sum-tar))
         {
             c+=hs.get(sum-tar);
         }
         hs.put(sum,hs.getOrDefault(sum,0)+1);
+        
         if(root.left!=null)
         helper(root.left,sum+root.left.val,tar,hs);
         if(root.right!=null)
         helper(root.right,sum+root.right.val,tar,hs);
+        
+        
         if(hs.containsKey(sum) && hs.get(sum)>1)
             hs.put(sum,hs.get(sum)-1);
         else
