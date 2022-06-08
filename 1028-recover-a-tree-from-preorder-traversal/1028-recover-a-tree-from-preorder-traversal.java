929f7c579f87f2c99a139a8345956318d5c257a5
Rohit <rv432233@gmail.com>
2022-06-08 16:13:32 +0530

    Time: 5 ms (78.85%), Space: 46.6 MB (37.76%) - LeetHub

--- 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.java
+++ 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.java
@@ -15,32 +15,25 @@
  */
 class Solution {
     int i=0;
-    public TreeNode recoverFromPreorder(String traversal) {
-        return helper(traversal,0);
+    public TreeNode recoverFromPreorder(String tra) {
+        return helper(tra,0);
     }
-    public TreeNode helper(String str,int depth)
+    public TreeNode helper(String tra,int depth)
     {
         int j=0;
-        while(i+j<str.length() && str.charAt(i+j)=='-')
-        {
+        while(i+j<tra.length() && tra.charAt(i+j)=='-')
             j++;
-        }
         if(j!=depth)
-        {
             return null;
-        }
         int k=0;
-        while(i+j+k<str.length() && str.charAt(i+j+k)!='-')
-        {
+        while(i+j+k<tra.length() && tra.charAt(i+j+k)!='-')
             k++;
-        }
-        int val=Integer.parseInt(str.substring(i+j,i+j+k));
+        int val=Integer.parseInt(tra.substring(i+j,i+j+k));
         i=i+j+k;
-        TreeNode node=new TreeNode(val);
+        TreeNode root=new TreeNode(val);
+        root.left=helper(tra,depth+1);
+        root.right=helper(tra,depth+1);
         
-        node.left=helper(str,depth+1);
-        node.right=helper(str,depth+1);
-        
-        return node;
+        return root;
     }
 }
