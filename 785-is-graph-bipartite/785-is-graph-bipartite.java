69bf987519a5d1fe8b7396299024dfd8cd1b6885
Rohit <rv432233@gmail.com>
2022-06-15 17:14:33 +0530

    Time: 2 ms (52.92%), Space: 54.3 MB (13.89%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,28 +1,29 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        int col=-1;
-        int []color=new int[graph.length];
-        for(int i=0;i<graph.length;i++)
+        int n=graph.length;
+        int []color=new int[n];
+        for(int i=0;i<n;i++)
         {
             if(color[i]==0)
             {
                 Queue<Integer> mq=new LinkedList<>();
-                mq.add(i);
-                color[i]=1;
-                while(mq.size()>0)
+            mq.add(i);
+            color[i]=1;
+            while(mq.size()>0)
+            {
+                int val=mq.remove();
+                
+                for(int x:graph[val])
                 {
-                    int val=mq.remove();
-                    for(int x:graph[val])
+                    if(color[x]==0)
                     {
-                        if(color[x]==0)
-                        {
-                            color[x]=-color[val];
-                            mq.add(x);
-                        }
-                        else if(color[x]==color[val])
-                            return false;
+                        color[x]=-color[val];
+                        mq.add(x);
                     }
+                    else if(color[x]==color[val])
+                        return false;
                 }
+            }
             }
         }
         return true;
