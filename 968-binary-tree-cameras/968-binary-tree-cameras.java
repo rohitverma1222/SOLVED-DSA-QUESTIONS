116b2bfa1c9accb2e436c0c4fe87de5b4877fac3
Rohit <rv432233@gmail.com>
2022-06-17 16:48:24 +0530

    Time: 2 ms (20.45%), Space: 44 MB (23.43%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.java
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -1,31 +1,72 @@
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+    class pair{
+        boolean camera;
+        boolean moniter;
+        boolean non_moniter;
+        pair(){
+            
+        }
+        pair(boolean camera,boolean moniter,boolean non_moniter)
+        {
+            this.camera=camera;
+            this.moniter=moniter;
+            this.non_moniter=non_moniter;
+        }
+    }
     int c=0;
-    final int moniter=1;
-    final int camera=2;
-    final int nonmonitered=3;
-    
     public int minCameraCover(TreeNode root) {
-        int val=helper(root);
-        if(val==3)
+        if(root==null)
+            return 0;
+        if(root!=null && root.right==null && root.left==null)
+            return 1;
+        pair get=helper(root);
+        if(get.non_moniter==true)
             return c+1;
         return c;
     }
-    public int helper(TreeNode root)
+    public pair helper(TreeNode root)
     {
         if(root==null)
-            return 1;
-
-        int left=helper(root.left);
-        int right=helper(root.right);
-        if(left==1 && right==1)
-            return 3;
-        else if(left==3 || right==3)
         {
-            c++;
-            return 2;
+            return new pair(false,true,false);
         }
-        else 
-            return 1;
+
+        pair left=helper(root.left);
+        pair right=helper(root.right);
+        pair newPair=new pair();
+        if(left.non_moniter==true || right.non_moniter==true)
+        {
+            newPair.non_moniter=false;
+            newPair.camera=true;
+            newPair.moniter=false;
+            c++;
+        }
+        else if(left.moniter==true && right.moniter==true)
+        {
+            newPair.non_moniter=true;
+            newPair.camera=false;
+            newPair.moniter=false;
+        }
+        else{
+            newPair.non_moniter=false;
+            newPair.camera=false;
+            newPair.moniter=true;
+        }
+        return newPair;
     }
 }
