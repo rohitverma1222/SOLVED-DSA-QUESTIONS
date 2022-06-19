33bf5544a0f7c473509a4f02aa7d1a375e6156cd
Rohit <rv432233@gmail.com>
2022-06-19 15:18:28 +0530

    Time: 4 ms (52.13%), Space: 47.8 MB (29.52%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -15,10 +15,9 @@ class Solution {
         {
             for(int j=0;j<grid[0].length;j++)
             {
-                if(grid[i][j]==1)
+                if(grid[i][j]==1 && visited[i][j]==false)
                 {
                     int m=bfs(grid,i,j,visited);
-                    // visited[i][j]=true;
                     max=Math.max(m,max);
                 }
             }
@@ -26,37 +25,25 @@ class Solution {
        
         return max;
     }
+    int [][]dir={{1,0},{-1,0},{0,-1},{0,1}};
     public int bfs(int [][]grid,int i,int j,boolean [][]visited)
     {
-        int ans=1;
-        Queue<pair> mq=new LinkedList<>();
-         int n=grid.length;
-        int m=grid[0].length;
-        int [][]dir={{-1,0},{1,0},{0,-1},{0,1}};
-        mq.add(new pair(i,j));
-        grid[i][j]=2;
-        while(mq.size()>0)
-        {
-            int size=mq.size();
-            while(size-->0)
-            {
-                pair rem=mq.remove();
-                
-                for(int []d:dir)
-                {
-                    int ni=rem.x+d[0];
-                    int nj=rem.y+d[1];
-                    if(ni>=0 && nj>=0 && ni<n && nj<m && grid[ni][nj]==1)
-                    {
-                        mq.add(new pair(ni,nj));
-                        grid[ni][nj]=2;
-                        ans++;
-                        // System.out.println(ans+" "+ni+" "+nj);
-                    }
-                }
-            }
-        }
-        return ans;
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]!=1 || visited[i][j]==true)
+            return 0;
+        int ans=0;
+        visited[i][j]=true;
+        
+        ans+=bfs(grid,i+1,j,visited);
+        ans+=bfs(grid,i-1,j,visited);
+        ans+=bfs(grid,i,j+1,visited);
+        ans+=bfs(grid,i,j-1,visited);
+        
+        return ans+1;
     }
-    
 }
+
+
+
+
+
+
