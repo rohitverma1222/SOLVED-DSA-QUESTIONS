b9c5ce23120a8efe272dd89778af1b7dc1a8d89f
Rohit <rv432233@gmail.com>
2022-06-21 20:47:20 +0530

    Time: 16 ms (15.73%), Space: 65.8 MB (44.26%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,45 +1,41 @@
 class Solution {
-    boolean flag=true;
-    int len=0;
+    int c=0;
     public int numEnclaves(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
         boolean [][]visited=new boolean[n][m];
-        int c=0;
+        int k=0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(visited[i][j]==false && grid[i][j]==1)
                 {
-                    // flag=true;
-                    len=0;
-                    if(helper(grid,i,j,visited))
+                    c=0;
+                    if(enclaves(grid,visited,i,j))
                     {
-                        c+=len;
+                        k+=c;
                     }
-                    
                 }
             }
         }
-        return c;
+        return k;
     }
-    public boolean helper(int [][]grid,int i,int j,boolean [][]visited)
+    public boolean enclaves(int [][]grid,boolean [][]visited,int i,int j)
     {
-        if(i<0 || j<0 ||i>=grid.length ||j>=grid[0].length)
-        {
+        if(i<0 ||j<0 || i>=grid.length || j>=grid[0].length)
             return false;
-        }
         else if(visited[i][j]==true || grid[i][j]==0)
             return true;
         
+        c++;
         visited[i][j]=true;
-        len++;
-        boolean c1=helper(grid,i+1,j,visited);
-        boolean c2=helper(grid,i-1,j,visited);
-        boolean c3=helper(grid,i,j+1,visited);
-        boolean c4=helper(grid,i,j-1,visited);
-        if(!c1 || !c2 ||!c3 || !c4)
+        boolean c1= enclaves(grid,visited,i+1,j);
+        boolean c2=enclaves(grid,visited,i-1,j);
+         boolean c3=enclaves(grid,visited,i,j+1);
+       boolean c4= enclaves(grid,visited,i,j-1);
+        
+        if(c1==false ||!c2 || !c3 ||!c4)
             return false;
         return true;
     }
