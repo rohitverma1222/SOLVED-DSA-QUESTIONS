e371c4217399c106c6398de7bcd256eb190d5347
Rohit <rv432233@gmail.com>
2022-06-24 23:29:29 +0530

    Time: 2 ms (39.55%), Space: 43.7 MB (65.94%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.java
+++ 684-redundant-connection/684-redundant-connection.java
@@ -3,20 +3,22 @@ class Solution {
     int rank[];
     public int[] findRedundantConnection(int[][] edges) {
         int n=edges.length;
-        // int m=edges[0].length;
         parent=new int[n+1];
         rank=new int[n+1];
+        
         for(int i=1;i<n;i++)
         {
             parent[i]=i;
             rank[i]=0;
         }
+        
         for(int i=0;i<n;i++)
         {
             int ans[]=union(edges[i][0],edges[i][1]);
             if(ans.length>0)
             return ans;
         }
+        
         return new int[0];
     }
     public int find(int x)
@@ -24,6 +26,7 @@ class Solution {
         if(parent[x]==x)
             return x;
         int st=find(parent[x]);
+        // path Compression
         parent[x]=st;
         return st;
     }
@@ -32,7 +35,7 @@ class Solution {
     {
         int leadU=find(u);
         int leadV=find(v);
-        
+//         union by rank
         if(leadU!=leadV)
         {
             int rankU=rank[leadU];
@@ -51,8 +54,10 @@ class Solution {
                 parent[leadU]=leadV;
                 rank[leadV]++;
             }
+//             just returning empty array
             return new int[0];
         }
+//         If cycle is present then is parent is run
         else
         {
             return new int[]{u,v};
