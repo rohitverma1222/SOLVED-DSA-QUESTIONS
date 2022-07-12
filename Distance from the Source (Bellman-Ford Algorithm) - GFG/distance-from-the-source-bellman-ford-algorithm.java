674bea60f981409e1aaf5e1079c5e508592bb2ee
Rohit <rv432233@gmail.com>
2022-07-13 03:26:51 +0530

    Added solution - LeetHub

---
+++ Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.java
@@ -0,0 +1,87 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class DriverClass
+{
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String str[] = read.readLine().trim().split(" ");
+            int V = Integer.parseInt(str[0]);
+            int E = Integer.parseInt(str[1]);
+    
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            
+            int i=0;
+            while (i++<E) {
+                String S[] = read.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                int w = Integer.parseInt(S[2]);
+                ArrayList<Integer> t1 = new ArrayList<>();
+                t1.add(u);
+                t1.add(v);
+                t1.add(w);
+                adj.add(t1);
+            }
+            
+            int S = Integer.parseInt(read.readLine());
+            
+            Solution ob = new Solution();
+            
+            int[] ptr = ob.bellman_ford(V, adj, S);
+            
+            for(i=0; i<V; i++)
+                System.out.print(ptr[i] + " ");
+            System.out.println();
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+*   adj: vector of vectors which represents the graph
+*   S: source vertex to start traversing graph with
+*   V: number of vertices
+*/
+class Solution
+{
+    static int[] bellman_ford(int V, ArrayList<ArrayList<Integer>> adj, int S)
+    {
+        // Write your code here
+        
+        int []paths=new int[V];
+        Arrays.fill(paths,100000000);
+        paths[S]=0;
+        
+        for(int i=0;i<(V-1);i++)
+        {
+            for(int j=0;j<adj.size();j++)
+            {
+                int u=adj.get(j).get(0);
+                int v=adj.get(j).get(1);
+                int wt=adj.get(j).get(2);
+
+                if(paths[u]==100000000)
+                    continue;
+
+                else if(paths[u]+wt<paths[v]){
+                    paths[v]=paths[u]+wt;
+                }
+            }
+        }
+        // int ans[]=new int[V-1];
+        // for(int i=1;i<ans.length;i++)
+        // {
+        //     ans[i-1]=paths[i];
+        // }
+        return paths;
+    }
+}
