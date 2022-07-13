0881fa24e9f7dc41e1d1b6a902d734f2c24bbd41
Rohit <rv432233@gmail.com>
2022-07-13 22:25:56 +0530

    Time: 245 ms (25.50%), Space: 123.8 MB (49.80%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,6 +1,7 @@
 class Solution {
     public int minReorder(int n, int[][] connections) {
         List<List<Integer>> li=new ArrayList<>();
+        HashSet<String> hs=new HashSet<>();
         for(int i=0;i<n;i++)
         {
             li.add(new ArrayList<>());
@@ -9,23 +10,37 @@ class Solution {
         for(int []c:connections)
         {
             li.get(c[0]).add(c[1]);
-            li.get(c[1]).add(-c[0]);
-        }
-        
-        return dfs(li,0,-1);
-    }
-    public int dfs(List<List<Integer>> li,int src,int dest)
-    {
-        int c=0;
-        for(int val:li.get(src))
-        {
-           if(Math.abs(val)==dest)
-            continue;
+            li.get(c[1]).add(c[0]);
 
-            if(val>0)
-                c++;
-            c+=dfs(li,Math.abs(val),src); 
+            hs.add(c[0]+"->"+c[1]);
+        }
+
+        boolean []visited=new boolean[n];
+        Queue<Integer> mq=new LinkedList<>();
+        mq.add(0);
+        int c=0;
+        while(mq.size()>0)
+        {
+            int s=mq.size();
+            for(int i=0;i<s;i++)
+            {
+                int rem=mq.remove();
+
+                visited[rem]=true;
+
+                for(int v:li.get(rem))
+                {
+                    if(visited[v]==false)
+                    {
+                        if(hs.contains(rem+"->"+v))
+                            c++;
+                        mq.add(v);
+                    }
+                }
+            }
         }
         return c;
+        
     }
+
 }
