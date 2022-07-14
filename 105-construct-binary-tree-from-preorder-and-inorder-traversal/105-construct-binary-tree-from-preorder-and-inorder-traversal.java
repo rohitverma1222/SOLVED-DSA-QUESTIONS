61394f0884d578413cf957e90c6d32d7415f08fd
Rohit <rv432233@gmail.com>
2022-07-14 11:21:14 +0530

    Time: 5 ms (40.08%), Space: 44.3 MB (53.31%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,4 +1,6 @@
-/**
+
+
+    /**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
@@ -16,30 +18,25 @@
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         HashMap<Integer,Integer> hs=new HashMap<>();
+
         for(int i=0;i<inorder.length;i++)
         {
-            int val=inorder[i];
-            hs.put(val,i);
+            hs.put(inorder[i],i);
         }
-        // int n=preorder.length-1;
-        TreeNode root=construct(0,preorder.length-1,0,inorder.length-1,preorder,hs);
+        return construct(preorder,hs,0,preorder.length-1,0,inorder.length-1);
+    }
+    public TreeNode construct(int []preorder,HashMap<Integer,Integer> hs,int pre_lo,int pre_hi,int in_lo,int in_hi)
+    { 
+        if(pre_lo>pre_hi || in_lo>in_hi)
+            return null;
+
+        TreeNode root=new TreeNode(preorder[pre_lo]);
+        int lim=hs.get(preorder[pre_lo]);
+        int log=hs.get(preorder[pre_lo])-in_lo;
+
+        root.left=construct(preorder,hs,pre_lo+1,pre_lo+log,in_lo,lim-1);
+        root.right=construct(preorder,hs,pre_lo+log+1,pre_hi,lim+1,in_hi);
         return root;
     }
-    public TreeNode construct(int pre_lo,int pre_hi,int in_lo,int in_hi,int []preorder,HashMap<Integer,Integer> hs)
-    {
-        if(pre_lo>pre_hi || in_lo>in_hi)
-        {
-            return null;
-        }
-        
-        TreeNode node=new TreeNode();
-        node.val=preorder[pre_lo];
-        System.out.println(preorder[pre_lo]);
-        int lim=hs.getOrDefault(preorder[pre_lo],0);
-        int log=lim-in_lo;
-        node.left=construct(pre_lo+1,pre_lo+lim,in_lo,lim-1,preorder,hs);
-        node.right=construct(pre_lo+log+1,pre_hi,lim+1,in_hi ,preorder,hs);
-        
-        return node;
-    }
+    
 }
