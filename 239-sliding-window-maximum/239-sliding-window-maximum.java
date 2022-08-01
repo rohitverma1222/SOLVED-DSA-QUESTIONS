4916fbd06ea64b965023a77fa2cda89630efba86
Rohit <rv432233@gmail.com>
2022-08-01 11:08:53 +0530

    Time: 233 ms (10.81%), Space: 54.6 MB (99.77%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,34 +1,39 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
+        int n=nums.length;
         Stack<Integer> st=new Stack<>();
-        int res[]=new int[nums.length];
-        for(int i=nums.length-1;i>=0;i--)
+        st.push(nums.length-1);
+        int res[]=new int[n];
+        res[n-1]=nums.length;
+
+        for(int i=n-2;i>=0;i--)
         {
             while(st.size()>0 && nums[i]>=nums[st.peek()])
-            {
                 st.pop();
-            }
-            
+
             if(st.size()==0)
-                res[i]=nums.length;
+                res[i]=n;
             else
-                res[i]=st.peek();
-            
+                res[i]=st.peek();;
+
             st.push(i);
         }
-        
+
+
         int j=0;
-        int ans[]=new int[nums.length-k+1];
-        for(int i=0;i<=nums.length-k;i++){
+        int ans[]=new int[n-k+1];
+        for(int i=0;i<ans.length;i++)
+        {
             if(i>j)
                 j=i;
-            
+
             while(res[j]<i+k)
                 j=res[j];
-            
+
             ans[i]=nums[j];
-            // System.out.println(nums[j]);
         }
+
         return ans;
+        
     }
 }
