cdef1aeb1aa1836499ee3e9a950e4ea8dcc51f3e
Rohit <rv432233@gmail.com>
2022-08-21 17:31:49 +0530

    Time: 33 ms (100.00%), Space: 54.3 MB (100.00%) - LeetHub

--- 2384-largest-palindromic-number/2384-largest-palindromic-number.java
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.java
@@ -1,49 +1,38 @@
 class Solution {
     public String largestPalindromic(String num) {
-        HashMap<Integer,Integer> hs=new HashMap<>();
+        int arr[]=new int[10];
         for(int i=0;i<num.length();i++)
         {
-            int ch=Integer.parseInt(num.charAt(i)+"");
-            hs.put(ch,hs.getOrDefault(ch,0)+1);
-        }
-        String str="";
+            arr[num.charAt(i)-'0']++;
+        }       
+        StringBuilder ans=new StringBuilder();
         for(int i=9;i>=0;i--)
         {
-            if(hs.containsKey(i) && (hs.get(i)/2)>0)
+            if(ans.length()==0 && i==0)
+                continue;
+            int mod=arr[i]%2;
+            int size=arr[i]/2;
+            while(size-->0)
             {
-                int mod=hs.get(i)%2;
-                int size=hs.get(i)/2;
-                while(size-->0)
-                {
-                    str+=i;
-                }
-                hs.put(i,mod);
-                if(mod==0)
-                hs.remove(i);
+                ans.append(i+"");
             }
+            arr[i]=mod;
         }
-        // System.out.println(str);
-        int idx=0;
-        for(int i=0;i<str.length();i++)
-        {
-            if(str.charAt(i)!='0')
-            {
-                idx=i;
-                break;
-            }
-        }
-        str=str.substring(idx);
-        StringBuilder right=new StringBuilder(str);
+        
+        // ans=new StringBuilder(ans.substring(idx));
+        // System.out.println(ans);
+        StringBuilder ans2=new StringBuilder(ans);
+
         int max=-1;
-        for(int i:hs.keySet())
+        for(int i=9;i>=0;i--)
         {
-            max=Math.max(i,max);
+            if(arr[i]>0)
+            max=Math.max(max,i);
         }
         if(max!=-1)
-        str+=max;
-        if(str.charAt(0)!='0')
-        return str+right.reverse().toString();
-        else
-            return max==-1?0+"":max+"";
+        {
+            ans.append(max);
+        }
+        return ans+ans2.reverse().toString();
     }
 }
