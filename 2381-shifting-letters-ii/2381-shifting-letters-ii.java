9e9524e10ae070b4f28ef64bda4b9fde9dbf5730
Rohit <rv432233@gmail.com>
2022-08-26 15:45:01 +0530

    Time: 1791 ms (25.00%), Space: 640 MB (25.00%) - LeetHub

---
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String shiftingLetters(String ss, int[][] updates) {
+        char []ch=ss.toCharArray();
+        int []arr=new int[ss.length()];
+        for(int i=0;i<updates.length;i++)
+        {
+            int s=updates[i][0];
+            int e=updates[i][1];
+            int val=updates[i][2]==0?-1:1;
+            arr[s]+=val;
+            if(e+1<ss.length())
+            {
+                arr[e+1]+=-val;
+            }
+        }
+        int sum=0;
+        String ans="";
+        for(int i=0;i<arr.length;i++)
+        {
+            sum+=arr[i];
+            int NewChar=(((ch[i]-'a')+sum)%26);
+            if(NewChar<0)
+                NewChar+=26;
+
+            ans+=(char)('a'+NewChar);
+        }
+        return ans;
+    }
+}
