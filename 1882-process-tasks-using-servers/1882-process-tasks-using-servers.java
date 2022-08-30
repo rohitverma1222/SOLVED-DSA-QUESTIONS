983389183f1bb149e9d4a7fbaee422d6d8aecd8c
Rohit <rv432233@gmail.com>
2022-08-30 09:30:51 +0530

    Time: 529 ms (37.52%), Space: 185.9 MB (43.17%) - LeetHub

---
+++ 1882-process-tasks-using-servers/1882-process-tasks-using-servers.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] assignTasks(int[] servers, int[] tasks) {
+        PriorityQueue<int[]> free=new PriorityQueue<>((a,b)->{
+            if(a[0]!=b[0])
+                return a[0]-b[0];
+            return a[1]-b[1];
+        });
+        PriorityQueue<int[]> used=new PriorityQueue<>((a,b)->{
+            if(a[2]!=b[2])
+                return a[2]-b[2];
+            else if(a[0]!=b[0])
+                return a[0]-b[0];
+            else
+                return a[1]-b[1];
+        });
+
+        for(int i=0;i<servers.length;i++)
+        {
+            free.add(new int[]{servers[i],i,0});
+        }
+        int []res=new int[tasks.length];
+        for(int i=0;i<tasks.length;i++)
+        {
+            int t=tasks[i];
+            while(!used.isEmpty() && used.peek()[2]<=i)
+                free.add(used.remove());
+
+            if(free.isEmpty())
+            {
+                int []curr=used.remove();
+                res[i]=curr[1];
+                curr[2]+=t;
+                used.add(curr);
+            }
+            else{
+                int []curr=free.remove();
+                res[i]=curr[1];
+                curr[2]=i+t;
+                used.add(curr);
+            }
+        }
+        return res;
+    }
+}
