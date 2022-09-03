34abf965499d17b7b77ac7b3991267cd06919995
Rohit <rv432233@gmail.com>
2022-09-03 16:31:02 +0530

    Time: 80 ms (15.05%), Space: 53.9 MB (79.58%) - LeetHub

---
+++ 85-maximal-rectangle/85-maximal-rectangle.java
@@ -0,0 +1,75 @@
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        int h[]=new int[matrix[0].length];
+        int max=0;
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=0;j<matrix[0].length;j++)
+            {
+                h[j]=matrix[i][j]=='0'? 0 : h[j]+1;
+            }
+            max=Math.max(max,largestRectangleArea(h));
+        }
+        return max;
+    }
+    public int largestRectangleArea(int[] heights) {
+        if(heights.length==1)
+            return heights[0];
+        int larr[]=left(heights);
+        int rarr[]=right(heights);
+
+        int max=0;
+        for(int i=0;i<heights.length;i++)
+        {
+            max=Math.max(max,(rarr[i]-larr[i]-1)*heights[i]);
+        }
+
+        return max;
+    }
+    public int[] right(int []arr)
+    {
+        int n=arr.length;
+        int []ans=new int[n];
+        Stack<Integer> st=new Stack<>();
+        st.push(n-1);
+        ans[n-1]=n;
+        for(int i=n-2;i>=0;i--)
+        {
+            while(st.size()>0 && arr[i]<=arr[st.peek()])
+            {
+                st.pop();
+            }
+
+            if(st.size()==0)
+            {
+                ans[i]=n;
+            }
+            else{
+                ans[i]=st.peek();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+    public int[] left(int []arr)
+    {
+        int n=arr.length;
+        int []ans=new int[n];
+        Stack<Integer> st=new Stack<>();
+        st.push(0);
+        ans[0]=-1;
+        for(int i=1;i<n;i++)
+        {
+            while(st.size()>0 && arr[i]<=arr[st.peek()])
+                st.pop();
+
+            if(st.size()==0)
+                ans[i]=-1;
+            else
+                ans[i]=st.peek();
+
+            st.push(i);
+        }
+        return ans;
+    }
+}
