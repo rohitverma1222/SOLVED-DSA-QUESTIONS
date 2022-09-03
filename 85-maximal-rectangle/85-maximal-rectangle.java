bbfc463500636a61b5c5a5baa44c5e19a9a00c0d
Rohit <rv432233@gmail.com>
2022-09-03 16:42:31 +0530

    Time: 73 ms (22.02%), Space: 54.5 MB (55.62%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.java
+++ 85-maximal-rectangle/85-maximal-rectangle.java
@@ -8,24 +8,45 @@ class Solution {
             {
                 h[j]=matrix[i][j]=='0'? 0 : h[j]+1;
             }
-            max=Math.max(max,largestRectangleArea(h));
+            max=Math.max(max,largestRect(h));
         }
         return max;
     }
-    public int largestRectangleArea(int[] heights) {
-        if(heights.length==1)
-            return heights[0];
-        int larr[]=left(heights);
-        int rarr[]=right(heights);
+    public int largestRect(int []arr)
+    {
+        int larr[]=left(arr);
+        int rarr[]=right(arr);
 
         int max=0;
-        for(int i=0;i<heights.length;i++)
-        {
-            max=Math.max(max,(rarr[i]-larr[i]-1)*heights[i]);
-        }
+        for(int i=0;i<arr.length;i++)
+            max=Math.max(max,(rarr[i]-larr[i]-1)*arr[i]);
 
         return max;
     }
+
+    public int[] left(int []arr)
+    {
+        Stack<Integer> st=new Stack<>();
+        int n=arr.length;
+        int []ans=new int[n];
+        ans[0]=-1;
+        st.push(0);
+        for(int i=1;i<n;i++)
+        {
+            while(st.size()>0 && arr[i]<=arr[st.peek()])
+                st.pop();
+
+            if(st.size()==0)
+                ans[i]=-1;
+            else
+                ans[i]=st.peek();
+
+            st.push(i);
+        }
+        return ans;
+
+    }
+
     public int[] right(int []arr)
     {
         int n=arr.length;
@@ -36,35 +57,10 @@ class Solution {
         for(int i=n-2;i>=0;i--)
         {
             while(st.size()>0 && arr[i]<=arr[st.peek()])
-            {
                 st.pop();
-            }
 
             if(st.size()==0)
-            {
                 ans[i]=n;
-            }
-            else{
-                ans[i]=st.peek();
-            }
-            st.push(i);
-        }
-        return ans;
-    }
-    public int[] left(int []arr)
-    {
-        int n=arr.length;
-        int []ans=new int[n];
-        Stack<Integer> st=new Stack<>();
-        st.push(0);
-        ans[0]=-1;
-        for(int i=1;i<n;i++)
-        {
-            while(st.size()>0 && arr[i]<=arr[st.peek()])
-                st.pop();
-
-            if(st.size()==0)
-                ans[i]=-1;
             else
                 ans[i]=st.peek();
 
