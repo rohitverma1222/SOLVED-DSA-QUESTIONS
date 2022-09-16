6b4c4e3d607ecfa71e46bacfc28db4321bc30ce1
Rohit <rv432233@gmail.com>
2022-09-16 13:24:54 +0530

    Time: 372 ms (21.94%), Space: 63.1 MB (28.07%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,35 +1,36 @@
 class Solution {
     public String longestPalindrome(String s) {
-        boolean [][]dp=new boolean[s.length()][s.length()];
-        int c=0;
-        int i=0;
-        int j=0;
-        for(int g=0;g<dp.length;g++)
+        int n=s.length();
+
+        boolean [][]dp=new boolean[n][n];
+        int i=0,j=0;
+        for(int g=0;g<dp[0].length;g++)
         {
-            for(int si=0,ei=g;ei<dp.length;ei++,si++)
+            for(int si=0,ei=g;ei<dp[0].length;ei++,si++)
             {
                 if(g==0)
                 {
                     dp[si][ei]=true;
-                    i=si;
-                    j=ei;
+
                 }
-                else if(g==1 && s.charAt(si)==s.charAt(ei))
-                {
-                    dp[si][ei]=true;
-                    i=si;
-                    j=ei;
+                else if(g==1){
+                    dp[si][ei]=s.charAt(si)==s.charAt(ei);
+                    if(dp[si][ei])
+                        {
+                            i=si;
+                            j=ei;
+                        }
                 }
                 else{
                     if(s.charAt(si)==s.charAt(ei))
                     {
                         dp[si][ei]=dp[si+1][ei-1] && true;
-                    }
-                    if(dp[si][ei])
+                        if(dp[si][ei])
                         {
                             i=si;
-                    j=ei;
+                            j=ei;
                         }
+                    }
                 }
             }
         }
