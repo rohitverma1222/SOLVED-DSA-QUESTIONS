5f95bc687b25fad7d8a6affb3b84ecb45677a125
Rohit <rv432233@gmail.com>
2022-09-27 23:00:15 +0530

    Time: 55 ms (84.66%), Space: 90.8 MB (95.33%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.java
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.java
@@ -1,80 +1,82 @@
 class Solution {
-	int parent[];
-	int rank[];
+    int []rank;
+    int []parent;
     public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
         parent=new int[s.length()+1];
         rank=new int[s.length()+1];
-        for(int i=0;i<s.length()+1;i++)
+
+        for(int i=0;i<s.length();i++)
         {
-        	parent[i]=i;
-        	rank[i]=0;
+            parent[i]=i;
+            rank[i]=0;
         }
 
         for(List<Integer> li:pairs)
         {
-        	int u=li.get(0);
-        	int v=li.get(1);
-
-        	union(u,v);
+            union(li.get(0),li.get(1));
         }
 
-         HashMap<Integer,PriorityQueue<Character>> hs=new HashMap<>();
+        HashMap<Integer,PriorityQueue<Character>> hs=new HashMap<>();
 
-         for(int i=0;i<s.length();i++)
-         {
-         	int lead=find(i);
-         	char ch=s.charAt(i);
-         	if(hs.containsKey(lead))
-         	{
+        for(int i=0;i<s.length();i++)
+        {
+            int lead=find(i);
+            char ch=s.charAt(i);
+
+            if(hs.containsKey(lead)==false)
+            {
+                PriorityQueue<Character> pq=new PriorityQueue<>();
+                pq.add(ch);
+                hs.put(lead,pq);
+            }
+            else{
                 PriorityQueue<Character> pq=hs.get(lead);
                 pq.add(ch);
-         		hs.put(lead,pq);
-         	}
-         	else{
-         		PriorityQueue<Character> pq=new PriorityQueue<>();
-         		pq.add(ch);
-         		hs.put(lead,pq);
-         	}
-         }
-         String str="";
-         for(int i=0;i<s.length();i++)
-         {
-         	int lead=find(i);
+                hs.put(lead,pq);
+            }
+        }
 
-         	if(hs.containsKey(lead))
-         	{
-         		str+=hs.get(lead).remove();
-         	}
-         }
-         return str;
-
+        StringBuilder str=new StringBuilder();
+        for(int i=0;i<s.length();i++)
+        {
+            int lead=find(i);
+            if(hs.containsKey(lead))
+            {
+                str.append(hs.get(lead).remove());
+            }
+        }
+        return str.toString();
     }
-       public int find(int x)
-	{
-		if(parent[x]==x)
-			return x;
-		    int st= find(parent[x]);
-	       parent[x]=st;
-	       return st;
-	}
+
+    public int find(int x)
+    {
+        if(parent[x]==x)
+            return x;
+        int st=find(parent[x]);
+        return parent[x]=st;
+    }
     public void union(int u,int v)
     {
-    	int leadu=find(u);
-    	int leadv=find(v);
-    	if(leadu!=leadv)
-    	{
-    		if(rank[leadu]<rank[leadv])
-    		{
-    			parent[leadu]=leadv;
-    		}
-    		else if(rank[leadv]<rank[leadu])
-    		{
-    			parent[leadv]=leadu;
-    		}
-    		else{
-    			parent[leadu]=leadv;;
-    			rank[leadv]++;
-    		}
-    	}
+        int leadu=find(u);
+        int leadv=find(v);
+
+        if(leadu!=leadv)
+        {
+            int ranku=rank[leadu];
+            int rankv=rank[leadv];
+
+            if(rankv<ranku)
+            {
+                parent[leadu]=leadv;
+            }
+            else if(ranku<rankv)
+            {
+                parent[leadv]=leadu;
+            }
+            else{
+                parent[leadu]=leadv;
+                rank[leadv]++;
+            }
+        }
     }
 }
