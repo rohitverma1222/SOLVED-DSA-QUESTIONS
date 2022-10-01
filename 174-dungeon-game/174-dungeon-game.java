f296986c80f0a1417f003411d47119953ec771b2
Rohit <rv432233@gmail.com>
2022-10-01 12:41:54 +0530

    Time: 2 ms (75.55%), Space: 41.6 MB (97.22%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.java
+++ 174-dungeon-game/174-dungeon-game.java
@@ -3,27 +3,29 @@ class Solution {
         int n=dungeon.length;
         int m=dungeon[0].length;
         int [][]dp=new int[n][m];
+
         for(int i=n-1;i>=0;i--)
         {
             for(int j=m-1;j>=0;j--)
             {
-                int min=Integer.MAX_VALUE;
                 if(i==n-1 && j==m-1)
                 {
-                    min=1;
+                    if(dungeon[i][j]>0)
+                        dp[i][j]=1;
+                    else
+                        dp[i][j]=Math.abs(dungeon[i][j])+1;
                 }
                 else if(i==n-1)
                 {
-                    min=dp[i][j+1];
+                    dp[i][j]=Math.max(1,dp[i][j+1]-dungeon[i][j]);
                 }
                 else if(j==m-1)
                 {
-                    min=dp[i+1][j];
+                    dp[i][j]=Math.max(1,dp[i+1][j]-dungeon[i][j]);
                 }
                 else{
-                    min=Math.min(dp[i][j+1],dp[i+1][j]);
+                    dp[i][j]=Math.max(1,Math.min(dp[i+1][j],dp[i][j+1])-dungeon[i][j]);
                 }
-                dp[i][j]=Math.max(1,min-dungeon[i][j]);
             }
         }
         return dp[0][0];
