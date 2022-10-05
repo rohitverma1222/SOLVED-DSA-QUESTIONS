d6594e221fb7f1a3062427059ad6e6cdf93b0a24
Rohit <rv432233@gmail.com>
2022-10-05 22:44:29 +0530

    Time: 3 ms (6.33%), Space: 47.3 MB (38.70%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,34 +1,40 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int i=0;
-        int j=nums.length-1;
-        int p=-1;
-        boolean flag=false;
-        while(i<=j)
+        int first=-1;
+        int last=-1;
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right)
         {
-            int mid=(i+j)/2;
-            
-            if(nums[mid]==target)
+            int mid=(left+right)/2;
+            if(target<=nums[mid])
             {
-                flag=true;
-                p=mid;
-                break;
+                right=mid-1;
             }
-            else if(nums[mid]<target){
-                i=mid+1;
+            else{
+                left=mid+1;
             }
-            else
-                j=mid-1;
+            if(nums[mid]==target){
+                last=mid;
+            }
         }
-        i=j=p;
-        System.out.println(i+" "+j);
-        while(i>=0 && i<nums.length && nums[i]==nums[p])
-            i--;
-        
-        while(j>=0 && j<nums.length&&nums[j]==nums[p])
-            j++;
-        if(flag)
-            return new int[]{i+1,j-1};
-        return new int[]{-1,-1};
+        System.out.println(last);
+        left=0;
+        right=nums.length-1;
+        while(left<=right)
+        {
+            int mid=(left+right)/2;
+            if(target>=nums[mid])
+            {
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+            if(nums[mid]==target){
+                first=mid;
+            }
+        }
+        return new int[]{last,first};
     }
 }
