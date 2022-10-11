7fb5497a09843ecce1b90861dbf88d1850a39bb4
Rohit <rv432233@gmail.com>
2022-10-12 00:25:06 +0530

    Time: 32 ms (5.02%), Space: 93.3 MB (58.66%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.java
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.java
@@ -1,20 +1,45 @@
 class Solution {
+    // public boolean increasingTriplet(int[] nums) {
+//         int []tails=new int[nums.length];
+
+//         tails[0]=nums[0];
+//         int len=1;
+
+//         for(int i=1;i<nums.length;i++)
+//         {
+//             if(tails[len-1]<nums[i])
+//             {
+//                 len++;
+//                 tails[len-1]=nums[i];
+//             }
+//             else{
+//                 int idx=Arrays.binarySearch(tails,0,len-1,nums[i]);
+
+//                 if(idx<0)
+//                 {
+//                     idx=-idx;
+//                     idx-=1;
+//                 }
+//                 tails[idx]=nums[i];
+//             }
+//         }
+//         return len>2;
+    // }
     public boolean increasingTriplet(int[] nums) {
         int []tails=new int[nums.length];
-
-        tails[0]=nums[0];
         int len=1;
-
-        for(int i=1;i<nums.length;i++)
+        tails[len-1]=nums[0];
+        
+        for(int i=1;i<tails.length;i++)
         {
             if(tails[len-1]<nums[i])
             {
+                tails[len]=nums[i];
                 len++;
-                tails[len-1]=nums[i];
             }
             else{
                 int idx=Arrays.binarySearch(tails,0,len-1,nums[i]);
-
+                
                 if(idx<0)
                 {
                     idx=-idx;
