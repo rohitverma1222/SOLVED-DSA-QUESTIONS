6c38bcbb9caefe7b4cf904df36ad44c16c0beab3
Rohit <rv432233@gmail.com>
2022-10-12 08:55:30 +0530

    Time: 39 ms (49.88%), Space: 123.3 MB (48.21%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
@@ -9,16 +9,28 @@ class Solution {
             inOut[u]++;
             inOut[v]++;
         }
-        PriorityQueue<Long> pq=new PriorityQueue<>();
-
+        
+        // PriorityQueue<Long> pq=new PriorityQueue<>();
+        HashMap<Integer,Integer> hs=new HashMap<>();
+        int idx=0;
         for(int i:inOut)
-            pq.add((long)i);
-
-        long i=1;
-        long sum=0;
-        while(pq.size()!=0)
         {
-            sum+=pq.remove()*i++;
+            hs.put(i,hs.getOrDefault(i,0)+1);
+        }
+
+        long maxValue=1;
+        long sum=0;
+        for(int i=0;i<n;i++)
+        {
+            if(hs.containsKey(i))
+            {
+                int val=hs.get(i);
+                while(val-->0)
+                {
+                    sum+=i*maxValue++;
+                }
+            }
+            
         }
         return sum;
     }
