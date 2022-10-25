cdbe2f02d2a97ae2fc1e251a2890e5e6ebd5d14b
Rohit <rv432233@gmail.com>
2022-10-25 16:49:31 +0530

    Time: 192 ms (73.33%), Space: 68 MB (65.33%) - LeetHub

---
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countBadPairs = function(nums) {
+    var totalPair=(pair)=>{
+        return pair*(pair-1)/2;
+    }
+    
+    const map = new Map();
+    for(let i=0;i<nums.length;i++)
+        {
+            let val=i-nums[i];
+            if(map.has(val))
+                map.set(val,map.get(val)+1);
+            else
+                map.set(val,1);
+        }
+    
+    let sum=totalPair(nums.length);
+    
+    for(val of map.values())
+        sum-=totalPair(val);
+    return sum;
+};
