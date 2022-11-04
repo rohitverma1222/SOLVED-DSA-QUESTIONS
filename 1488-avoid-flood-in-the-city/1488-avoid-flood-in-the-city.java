5226cae7d23e71964e4de722a2d8a38b1415825d
Rohit <rv432233@gmail.com>
2022-11-04 21:19:27 +0530

    Time: 146 ms (72.17%), Space: 151.4 MB (48.32%) - LeetHub

---
+++ 1488-avoid-flood-in-the-city/1488-avoid-flood-in-the-city.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[] avoidFlood(int[] rains) {
+        HashMap<Integer,Integer> hs=new HashMap<>();
+        int []ans=new int[rains.length];
+        TreeSet<Integer> tr=new TreeSet<>();
+        // ans[0]=-1;
+        for(int i=0;i<rains.length;i++)
+        {
+            if(rains[i]==0)
+            {
+                tr.add(i);
+            }
+            else{
+                
+                if(hs.containsKey(rains[i]))
+                {
+                    int val=hs.get(rains[i]);
+                    
+                    
+                    Integer idx=tr.ceiling(val);
+                    if(idx==null)
+                        return new int[0];
+                    tr.remove(idx);
+                    ans[idx]=rains[i];
+                    hs.put(rains[i],i);
+                    ans[i]=-1;
+                }
+                else{
+                    ans[i]=-1;
+                    hs.put(rains[i],i);
+                }
+            }
+        }
+        for(int i=0;i<ans.length;i++)
+        {
+            if(ans[i]==0)
+                ans[i]=1;
+        }
+        return ans;
+    }
+}
