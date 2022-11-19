648c384689a41d55ac5fbc1d484b8f2e0ab13c1e
Rohit <rv432233@gmail.com>
2022-11-19 21:16:45 +0530

    Time: 1 ms (73.16%), Space: 41.7 MB (7.46%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -2,18 +2,31 @@ class Solution {
     public int uniquePaths(int n, int m) {
         int [][]dp=new int[n][m];
         int dir[][]={{1,0},{0,1}};
-        dp[n-1][m-1]=1;
-        for(int i=n-1;i>=0;i--)
+        
+        return mazePath(0,0,n-1,m-1,dp,dir);
+    }
+    public int mazePath(int sr,int sc,int er,int ec,int [][]dp,int [][]dir)
+    {
+        for(int i=er;i>=sr;i--)
         {
-            for(int j=m-1;j>=0;j--)
+            for(int j=ec;j>=sc;j--)
             {
+                if(i==er && j==ec)
+                {
+                    dp[i][j]=1;
+                    continue;
+                }
+
+                int count=0;
+
                 for(int d[]:dir)
                 {
                     int nr=d[0]+i;
                     int nc=d[1]+j;
-                    if(nr>=0 && nc>=0 && nr<n && nc<m)
-                        dp[i][j]+=dp[nr][nc];
+                    if(nr>=0 && nc>=0 && nr<dp.length && nc <dp[0].length)
+                        count+=dp[nr][nc];
                 }
+                dp[i][j]=count;
             }
         }
         return dp[0][0];
